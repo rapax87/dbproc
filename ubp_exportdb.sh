d031d9dba13541c01d62220d56ad9fcf707eafa3
Xu Bingbing <xu2bi@Yahoo.com>
2013-12-25 17:04:31 +0800

    Update ubp_exportdb.sh

--- ubp_exportdb.sh
+++ ubp_exportdb.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 #title           :ubp_exportdb.sh
-#description     :enable or disable an regular export task
+#description     :export DB data to XML
 #author          :xubingbing
 #date            :20131223
 #version         :0.1    
-#usage           :./ubp_exportdb.sh <option> <outputdir> <day> <month> <day of week>
+#usage           :./ubp_exportdb.sh <outputdir>
 #notes           :there should be a output.list file which specifies table names 
 #                 in output directory. 
 #==============================================================================
@@ -13,25 +13,51 @@ dir=$(dirname $0)
 cd $dir
 
 #if [ $# -lt 2 ];then
-if [ $# -lt 5 ];then
-  echo "Usage: $0 <option> <outputdir> <day> <month> <day of week>"
+if [ $# -lt 1 ];then
+  echo "Usage: $0 <outputdir>"
   exit 1
 fi
 
-option=$1
-outputdir=$2
-day=$3
-week=$5
-month=$4
+#tablename=$1
+outputdir=$1
 datestr=`date +'%Y%m%d%H%M%S'`
 
-if [ $option -eq 0 ];then
-  sed -i '/ubp_exportdb.sh/d' /var/spool/cron/tabs/root
-else
-  echo "0 0 $day $month $week /opt/UBP/bin/ubp_exportdb.sh $outputdir >/dev/null 2>&1 &" >> /var/spool/cron/tabs/root
+. ./ubp_dbcommon.sh
+
+if [ ! -d $outputdir ];then
+  output "ERROR" "$outputdir not exist" $LINENO
+  exit 2
 fi
-/etc/init.d/cron reload
-/etc/init.d/cron restart
+
+#. /opt/UBP/bin/common.sh
+dbname=$g_ubpdb
+userName=$g_odb_user
+dbpasswd=$g_odb_psw
+
+if [ ! -f ${outputdir}/output.list ];then
+  output "ERROR" "${outputdir}/output.list not exist" $LINENO
+  exit 3
+fi
+outputfile=${outputdir}/output_${dbname}_${datestr}.xml
+rm -rf $outputfile
+echo "<?xml version=\"1.0\"?>" >>$outputfile
+echo "<mysqldump xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" >>$outputfile
+echo "<database name=\"${dbname}\">" >>$outputfile
+
+cat ${outputdir}/output.list|while read tablename;do
+  if [ -z "$tablename" ];then
+    break
+  fi
+  mysqldump ${dbname} ${tablename} -X -u${userName} -p${dbpasswd} -t --skip-triggers > ${outputdir}/${tablename}.xml
+  sed '1,3d;N;$d;P;D' ${outputdir}/${tablename}.xml>>$outputfile
+  rm -rf ${outputdir}/${tablename}.xml
+done
+echo "</database>" >>$outputfile
+echo "</mysqldump>" >>$outputfile
+
+#tar zcvf ${outputdir}/output_${dbname}_${datestr}.tar.gz $outputfile
+#rm -rf $outputfile
+output "INFO" "Data exported into $outputfile successfully." $LINENO
 
 cd - > /dev/null 2>&1
 
