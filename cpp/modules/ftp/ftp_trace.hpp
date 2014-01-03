b7765c7e28edbcad309ff1ce2ac6bace2ffda435
Xu Bingbing <xu2bi@Yahoo.com>
2014-01-03 11:03:58 +0800

    Update ftp_trace.hpp

--- cpp/modules/ftp/ftp_trace.hpp
+++ cpp/modules/ftp/ftp_trace.hpp
@@ -1,4 +1,4 @@
-锘?********************************************************************
+/********************************************************************
   Filename:   ftp_trace.hpp
   Author:     
   Created:    
@@ -6,18 +6,19 @@
 
 *********************************************************************/
 
-#ifndef UBP_PLATFORM_MOUDLES_FTP_TRACE_HPP_
-#define UBP_PLATFORM_MOUDLES_FTP_TRACE_HPP_
+#ifndef PLATFORM_MOUDLES_FTP_TRACE_HPP_
+#define PLATFORM_MOUDLES_FTP_TRACE_HPP_
 
-#include "platform/modules/trace/ubp_trace.hpp"
-#include "platform/modules/error/ubp_error.hpp"
+#include "platform/modules/trace/trace.hpp"
+#include "platform/modules/error/error.hpp"
 
-#define FTP_TRACE_DEBUG(LOG) UBP_LOGGER_DEBUG("PLATFORM.FTP", LOG)
-#define FTP_TRACE_INFO(LOG) UBP_LOGGER_INFO("PLATFORM.FTP", LOG)
-#define FTP_TRACE_WARN(LOG)  UBP_LOGGER_WARN("PLATFORM.FTP", LOG)
-#define FTP_TRACE_ERROR(LOG)  UBP_LOGGER_ERROR("PLATFORM.FTP", LOG)
-#define FTP_TRACE_FATAL(LOG)  UBP_LOGGER_FATAL("PLATFORM.FTP", LOG)
+#define FTP_TRACE_DEBUG(LOG) LOGGER_DEBUG("PLATFORM.FTP", LOG)
+#define FTP_TRACE_INFO(LOG) LOGGER_INFO("PLATFORM.FTP", LOG)
+#define FTP_TRACE_WARN(LOG)  LOGGER_WARN("PLATFORM.FTP", LOG)
+#define FTP_TRACE_ERROR(LOG)  LOGGER_ERROR("PLATFORM.FTP", LOG)
+#define FTP_TRACE_FATAL(LOG)  LOGGER_FATAL("PLATFORM.FTP", LOG)
 
+//检查值是否合法，如果有错则打印错误并返回错误码
 #define FTP_TRACE_RETURN_ERRCODE_IF_FALSE(CHECK_VAL,RTN_ERRCODE,ERROR_DETAIL) \
 	do { if( true != CHECK_VAL) { \
 	FTP_TRACE_ERROR("Return code:"<< RTN_ERRCODE<< ","<<ERROR_DETAIL); \
@@ -25,13 +26,13 @@
 	} while (0)
 	
 #define FTP_TRACE_RETURN_ERRCODE_IF_ERROR(CHECK_VAL,RTN_ERRCODE,ERROR_DETAIL) \
-	do { if( UBP_SUCCESS != CHECK_VAL) { \
+	do { if( SUCCESS != CHECK_VAL) { \
 	FTP_TRACE_ERROR("Error code:"<< RTN_ERRCODE<< ","<<ERROR_DETAIL); \
 	return RTN_ERRCODE; }  \
 	} while (0)
 
 #define FTP_TRACE_RETURN_NULL_IF_ERROR(CHECK_VAL,RTN_ERRCODE,ERROR_DETAIL) \
-	do { if( UBP_SUCCESS != CHECK_VAL) { \
+	do { if( SUCCESS != CHECK_VAL) { \
 	FTP_TRACE_ERROR("Error code:"<< RTN_ERRCODE<< ","<<ERROR_DETAIL); \
 	return NULL; }  \
 	} while (0)
@@ -48,5 +49,5 @@
 	return NULL; }  \
 	} while (0)
 
-#endif //UBP_PLATFORM_MOUDLES_FTP_TRACE_HPP_
+#endif //PLATFORM_MOUDLES_FTP_TRACE_HPP_
 
